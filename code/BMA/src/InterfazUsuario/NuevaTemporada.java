4ecb279621423061c1ee530557adbcaa663f2473
Moisés Gautier Gómez <moisesgautiergomez@correo.ugr.es>
2013-05-24 20:56:41 +0200

    Añadiendo licencia GNU GPL v3 al codigo fuente de la interfaz en java

--- code/BMA/src/InterfazUsuario/NuevaTemporada.java
+++ code/BMA/src/InterfazUsuario/NuevaTemporada.java
@@ -18,6 +18,35 @@ import javax.swing.JOptionPane;
  *
  * @author Diego
  */
+
+/*
+ ******************************************************************************
+                   (c) Copyright 2013 
+                   * 
+                   * Moisés Gautier Gómez
+                   * Julio Ros Martínez
+                   * Francisco Javier Gómez del Olmo
+                   * Francisco Santolalla Quiñonero
+                   * Carlos Jesús Fernández Basso
+                   * Alexander Moreno Borrego
+                   * Jesús Manuel Contreras Siles
+                   * Diego Muñoz Rio
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+ */
+
 public class NuevaTemporada extends javax.swing.JFrame {
     
     private PantallaPrincipal creador;
