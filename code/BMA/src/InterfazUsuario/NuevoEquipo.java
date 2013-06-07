0bd8bbb312f66316b72422c1fb3b83c70b91edad
Alex Moreno <alexmobo@gmail.xom>
2013-06-07 10:55:15 +0200

    Acabada la interfaz por fin

--- code/BMA/src/InterfazUsuario/NuevoEquipo.java
+++ code/BMA/src/InterfazUsuario/NuevoEquipo.java
@@ -4,14 +4,10 @@
  */
 package InterfazUsuario;
 
-import GestionDeAlumnos.Alumno;
 import GestionDeAlumnos.GestorAlumnos;
-import GestionDeCategorias.GestorCategorias;
 import GestionDeEquipos.GestorEquipos;
 import ServiciosAlmacenamiento.BaseDatos;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -655,42 +651,7 @@ public class NuevoEquipo extends javax.swing.JFrame {
     private void comSexoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comSexoActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_comSexoActionPerformed
-    /**/
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NuevoEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NuevoEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NuevoEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NuevoEquipo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NuevoEquipo().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Error;
     private javax.swing.JCheckBox PerteneceFundacion;
